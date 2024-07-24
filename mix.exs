c1121edad80a57f17d46a7ff5e1cb945ebb386a6
lalabuy948 <lalabuy9948@gmail.com>
2024-07-24 18:54:39 +0200

    further clean up from ecto

--- mix.exs
+++ mix.exs
@@ -34,8 +34,6 @@ defmodule TheWorldLine.MixProject do
     [
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
-      {:ecto_sql, "~> 3.10"},
-      {:ecto_sqlite3, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
